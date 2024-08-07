94ee63f1a2d1626eb7a15819e378f89f973dd346
HenryCROSS <henry583050973@gmail.com>
2024-08-07 01:14:48 -0400

    update tool

--- analysis_tools/auto_detection.py
+++ analysis_tools/auto_detection.py
@@ -1,3 +1,4 @@
+import csv
 import cv2
 import numpy as np
 import os
@@ -5,9 +6,14 @@ from typing import List, Tuple
 import multiprocessing
 import psutil
 from functools import reduce
+import time
 
 IMG_DIR_IN: str = "./Pictures"
 IMG_DIR_OUT: str = "./Pictures_1_out"
+CSV_FILE: str = "output_pixel_counts.csv"
+
+# Global lock for synchronization
+lock = multiprocessing.Lock()
 
 
 class Image:
@@ -115,37 +121,38 @@ def mask_with_shape(mask_img):
 
 
 def do_erosion(img: np.ndarray, kernel_size: Tuple[int, int] = (5, 5), kernel_shape: str = 'rect') -> np.ndarray:
-    """
-    Apply erosion to the input image with a customizable kernel.
+  """
+  Apply erosion to the input image with a customizable kernel.
 
-    Parameters:
-    - img: Input image (single channel or BGR).
-    - kernel_size: Size of the erosion kernel (width, height).
-    - kernel_shape: Shape of the erosion kernel. Options are 'rect', 'ellipse', and 'cross'.
+  Parameters:
+  - img: Input image (single channel or BGR).
+  - kernel_size: Size of the erosion kernel (width, height).
+  - kernel_shape: Shape of the erosion kernel. Options are 'rect', 'ellipse', and 'cross'.
 
-    Returns:
-    - Eroded image.
-    """
-    # Create the kernel based on the specified shape
-    if kernel_shape == 'rect':
-        kernel = np.ones(kernel_size, np.uint8)
-    elif kernel_shape == 'ellipse':
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel_size)
-    elif kernel_shape == 'cross':
-        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, kernel_size)
-    else:
-        raise ValueError("Invalid kernel_shape. Options are 'rect', 'ellipse', and 'cross'.")
+  Returns:
+  - Eroded image.
+  """
+  # Create the kernel based on the specified shape
+  if kernel_shape == 'rect':
+    kernel = np.ones(kernel_size, np.uint8)
+  elif kernel_shape == 'ellipse':
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel_size)
+  elif kernel_shape == 'cross':
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, kernel_size)
+  else:
+    raise ValueError(
+        "Invalid kernel_shape. Options are 'rect', 'ellipse', and 'cross'.")
 
-    # Apply erosion
-    eroded_img = cv2.erode(img, kernel, iterations=1)
-    return eroded_img
+  # Apply erosion
+  eroded_img = cv2.erode(img, kernel, iterations=1)
+  return eroded_img
 
 
 def cvt_black2white(img):
   if len(img.shape) == 2:
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-  black_pixels = np.where((img[:, :, 0] == 0) & (
-      img[:, :, 1] == 0) & (img[:, :, 2] == 0))
+  black_pixels = np.where((img[:, :, 0] == 0) &
+                          (img[:, :, 1] == 0) & (img[:, :, 2] == 0))
   img[black_pixels] = [255, 255, 255]
   return img
 
@@ -157,7 +164,8 @@ def cvt_BGR2HSV(img):
 def do_img_segmentation(img):
   Z = img.reshape((-1, 3))
   Z_f32 = np.float32(Z)
-  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+  criteria = (cv2.TERM_CRITERIA_EPS +
+              cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
   K = 8
   _, label, center = cv2.kmeans(
       Z_f32, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
@@ -182,6 +190,7 @@ def do_otsu_thresholding(img):
   _, th3 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
   return th3
 
+
 def apply_mosaic_min(image, block_size: int):
   if block_size < 1:
     block_size = 1
@@ -194,72 +203,75 @@ def apply_mosaic_min(image, block_size: int):
       image[y:y + block_size, x:x + block_size] = (B, G, R)
   return image
 
+
 def enhance_contrast_histogram_equalization(img: np.ndarray) -> np.ndarray:
-    """
-    Enhance the contrast of an image using Histogram Equalization.
+  """
+  Enhance the contrast of an image using Histogram Equalization.
 
-    Args:
-    - img: Input image (BGR or grayscale).
+  Args:
+  - img: Input image (BGR or grayscale).
 
-    Returns:
-    - Enhanced contrast image.
-    """
-    # Convert the image to grayscale if it is in BGR format
-    if len(img.shape) == 3 and img.shape[2] == 3:
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    else:
-        img_gray = img
+  Returns:
+  - Enhanced contrast image.
+  """
+  # Convert the image to grayscale if it is in BGR format
+  if len(img.shape) == 3 and img.shape[2] == 3:
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  else:
+    img_gray = img
 
-    # Apply Histogram Equalization
-    enhanced_img = cv2.equalizeHist(img_gray)
+  # Apply Histogram Equalization
+  enhanced_img = cv2.equalizeHist(img_gray)
 
-    # If the input image was in BGR, convert the enhanced grayscale image back to BGR
-    if len(img.shape) == 3 and img.shape[2] == 3:
-        enhanced_img = cv2.cvtColor(enhanced_img, cv2.COLOR_GRAY2BGR)
+  # If the input image was in BGR, convert the enhanced grayscale image back to BGR
+  if len(img.shape) == 3 and img.shape[2] == 3:
+    enhanced_img = cv2.cvtColor(enhanced_img, cv2.COLOR_GRAY2BGR)
 
-    return enhanced_img
+  return enhanced_img
+
 
 def filter_contours_by_area(image: np.ndarray, min_area: int, fill: bool = True) -> np.ndarray:
-    """
-    Filter contours by area but keep smaller contours within larger contours.
+  """
+  Filter contours by area but keep smaller contours within larger contours.
 
-    Parameters:
-    - image: Input binary image (single channel).
-    - min_area: Minimum area of contours to retain.
-    - fill: Whether to fill the interior of the contours. Default is True.
+  Parameters:
+  - image: Input binary image (single channel).
+  - min_area: Minimum area of contours to retain.
+  - fill: Whether to fill the interior of the contours. Default is True.
 
-    Returns:
-    - Filtered image containing only contours with area greater than or equal to min_area,
-      but also retains smaller contours within larger contours.
-    """
-    # Find all external contours and hierarchy
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    filtered_img = np.zeros_like(image)
-    
-    # Create a list to store the contours to keep
-    contours_to_keep = []
+  Returns:
+  - Filtered image containing only contours with area greater than or equal to min_area,
+    but also retains smaller contours within larger contours.
+  """
+  # Find all external contours and hierarchy
+  contours, hierarchy = cv2.findContours(
+      image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  filtered_img = np.zeros_like(image)
 
-    # Iterate through the contours and hierarchy
-    for i, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        parent_index = hierarchy[0][i][3]
+  # Create a list to store the contours to keep
+  contours_to_keep = []
 
-        if area >= min_area or parent_index != -1:
-            contours_to_keep.append(contour)
+  # Iterate through the contours and hierarchy
+  for i, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    parent_index = hierarchy[0][i][3]
 
-    # Draw the contours on the new image
-    for contour in contours_to_keep:
-        if fill:
-            cv2.drawContours(filtered_img, [contour], -1, 255, thickness=cv2.FILLED)
-        else:
-            cv2.drawContours(filtered_img, [contour], -1, 255, thickness=1)
+    if area >= min_area or parent_index != -1:
+      contours_to_keep.append(contour)
 
-    return filtered_img
+  # Draw the contours on the new image
+  for contour in contours_to_keep:
+    if fill:
+      cv2.drawContours(filtered_img, [contour], -1, 255, thickness=cv2.FILLED)
+    else:
+      cv2.drawContours(filtered_img, [contour], -1, 255, thickness=1)
+
+  return filtered_img
+
 
 def process_image_larger_shape(img):
   img_color = img.copy()
   img = cv2.bitwise_not(img)
-
 
   # mosaic_min_img = apply_mosaic_min(img, 3)
   blurred_img = cv2.GaussianBlur(img, (7, 7), 0)
@@ -379,7 +391,7 @@ def draw_mask_contour(img, img_overlapped, bgr=(0, 255, 0), thickness=1):
   return img
 
 
-def process_and_save_image(image: Image, out_dir: str):
+def process_and_save_image(image: Image, out_dir: str, csv_file: str):
   img = image.get_img()
   if img is None:
     print(f"Failed to load image at {os.path.join(image.path, image.filename)}")
@@ -388,24 +400,35 @@ def process_and_save_image(image: Image, out_dir: str):
   preprocessed_img_with_selection, preprocessed_mask = process_image_larger_shape(
       img)
 
-  rect_mask = generate_center_mask(
-      img, 3, 10, 3, 3)
+  rect_mask = generate_center_mask(img, 3, 10, 3, 3)
   img_mask = cv2.bitwise_and(rect_mask, rect_mask, mask=preprocessed_mask)
 
   result_mask = detail_process_img(img, img_mask)
 
   img_labelled = draw_overlap_transparently(img, result_mask, 0.25, (0, 0, 255))
-#   img_labelled = draw_mask_contour(img_labelled, preprocessed_mask, (235, 32, 37), 2)
+  # img_labelled = draw_mask_contour(img_labelled, preprocessed_mask, (235, 32, 37), 2)
   img_labelled = draw_mask_contour(img_labelled, result_mask, (0, 0, 255), 2)
-#   img_labelled = draw_mask_contour(img_labelled, rect_mask, (235, 0, 235), 2)
+  # img_labelled = draw_mask_contour(img_labelled, rect_mask, (235, 0, 235), 2)
 
   output_path = os.path.join(out_dir, image.filename)
   cv2.imwrite(output_path, result_mask)
   cv2.imwrite(f"{output_path}_label.jpg", img_labelled)
   print(f"Processed image saved at {output_path}")
 
+  # Calculate white pixel count
+  white_pixel_count = np.sum(result_mask == 255)
 
-def main(in_dir: str, out_dir: str) -> None:
+  # Save the count to the CSV file
+  with lock:
+    file_exists = os.path.isfile(csv_file)
+    with open(csv_file, mode='a', newline='') as file:
+      writer = csv.writer(file)
+      if not file_exists:
+        writer.writerow(["filename", "white_pixel_count"])
+      writer.writerow([image.filename, white_pixel_count])
+
+
+def main(in_dir: str, out_dir: str, csv_file: str) -> None:
   if not os.path.exists(in_dir):
     print("Input directory does not exist")
     return
@@ -421,8 +444,43 @@ def main(in_dir: str, out_dir: str) -> None:
 
   with multiprocessing.Pool(num_processes) as pool:
     pool.starmap(process_and_save_image, [
-                 (img, out_dir) for img in images])
+                 (img, out_dir, csv_file) for img in images])
+
+
+def get_unique_csv_file_path(base_path: str, base_name: str) -> str:
+  """
+  Generate a unique CSV file path by adding a number suffix if the file already exists.
+
+  :param base_path: The directory where the file will be created.
+  :param base_name: The base name of the file.
+  :return: A unique file path.
+  """
+  file_path = os.path.join(base_path, base_name)
+  if not os.path.exists(file_path):
+    return file_path
+
+  file_root, file_ext = os.path.splitext(base_name)
+  counter = 1
+  while True:
+    new_file_name = f"{file_root}_{counter}{file_ext}"
+    new_file_path = os.path.join(base_path, new_file_name)
+    if not os.path.exists(new_file_path):
+      return new_file_path
+    counter += 1
 
 
 if __name__ == "__main__":
-  main(IMG_DIR_IN, IMG_DIR_OUT)
+  csv_file_path = get_unique_csv_file_path(IMG_DIR_OUT, CSV_FILE)
+  start_time = time.time()  # Start the timer
+  main(IMG_DIR_IN, IMG_DIR_OUT, csv_file_path)
+  end_time = time.time()  # End the timer
+  total_time = end_time - start_time  # Calculate total time
+
+  print(f"Finished in {total_time}s!")
+
+  # Save the total time to the CSV file
+  with lock:
+    with open(csv_file_path, mode='a', newline='') as file:
+      writer = csv.writer(file)
+      writer.writerow(["Total Time", total_time])
+      print(f"File: {csv_file_path} is created!")
